eb54f6050c980e229f0c39fa9d1184e5300b645c
HeyItzKillerMC <39652323+N0RTHSIDE@users.noreply.github.com>
2023-05-01 01:10:42 -0400

    Update Main.php

--- src/HeyItzKillerMC/Main.php
+++ src/HeyItzKillerMC/Main.php
@@ -26,7 +26,7 @@ class Main extends PluginBase implements Listener{
             $killer = $cause->getDamager();
             $killer_pots = count($killer->getInventory()->all(\pocketmine\item\Item::get(438, 22)));
             $message = TextFormat::GREEN . $killer->getName() . TextFormat::DARK_GREEN . "[" . $killer_pots . "] " . TextFormat::GRAY . "killed " . TextFormat::RED . $looser . TextFormat::DARK_RED . "[" . $looser_pots . "]";
-            $this->getServer()->broadcastMessage($message);
+            $event->setDeathMessage($message);
         }
     }
 }
