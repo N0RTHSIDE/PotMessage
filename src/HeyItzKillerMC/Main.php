2e6f88f04b2e31a893fc532f9db38ab39d8db5cb
HeyItzKillerMC <39652323+N0RTHSIDE@users.noreply.github.com>
2023-05-01 00:38:22 -0400

    Update Main.php

--- src/HeyItzKillerMC/Main.php
+++ src/HeyItzKillerMC/Main.php
@@ -5,26 +5,26 @@ namespace HeyItzKillerMC;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\PlayerDeathEvent;
 use pocketmine\utils\TextFormat;
-use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
 
 class Main extends PluginBase implements Listener{
 
-    public function onEnable(): void{
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvent(PlayerDeathEvent::class, $this);
     }
 
-    public function onPlayerDeath(PlayerDeathEvent $event): void{
+    public function onPlayerDeath(PlayerDeathEvent $event){
         $player = $event->getPlayer();
         $looser = $player->getName();
-        $looser_pots = count($player->getInventory()->all(Item::get(438, 22))); // 438:22 is the item ID of the potion
+        $looser_pots = count($player->getInventory()->all(ItemFactory::getInstance()->get(438, 22))); // 438:22 is the item ID of the potion
 
         $cause = $player->getLastDamageCause();
 
         if($cause instanceof EntityDamageByEntityEvent){
             $killer = $cause->getDamager();
-            $killer_pots = count($killer->getInventory()->all(Item::get(438, 22)));
+            $killer_pots = count($killer->getInventory()->all(ItemFactory::getInstance()->get(438, 22)));
             $message = TextFormat::GREEN . $killer->getName() . TextFormat::DARK_GREEN . "[" . $killer_pots . "] " . TextFormat::GRAY . "killed " . TextFormat::RED . $looser . TextFormat::DARK_RED . "[" . $looser_pots . "]";
             $this->getServer()->broadcastMessage($message);
         }
